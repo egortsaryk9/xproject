716edb0a67baa79832b2a515b59d306002a6a3ed
egortsaryk9 <egortsaryk@gmail.com>
2017-02-11 02:26:39 +0300

    Core module is added to 'xproject' app

---
+++ src/Core/DatabaseService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class PostModel 
+    {
+        public int _id {get; set;}
+        public string name {get; set;}
+        public string website {get; set;}
+        public string description {get; set;}
+    }
+
+    public class DatabaseService
+    {
+        public List<PostModel> GetPosts()
+        {
+            return _posts;
+        }
+
+        public PostModel FindById(int id)
+        {
+            return _posts.Where(x => x._id == id).First();
+        }
+
+        public void Create(PostModel post)
+        {
+            post._id = _posts.Last()._id + 1;
+            _posts.Add(post);
+        }
+
+        public void Update(int id, string name, string description, string website)
+        {
+            var post = _posts.Where(x => x._id == id).First(); 
+            post.description = description;
+            post.name = name;
+            post.website = website;
+        }
+
+
+        private static List<PostModel> _posts = new List<PostModel>()
+        {
+            new PostModel()
+            {
+                _id = 1,
+                name = "Angular",
+                website = "https://angular.io/",
+                description = "Angular is a development platform for building mobile and desktop web applications."
+            },
+            new PostModel()
+            {
+                _id = 2,
+                name = "RxJs",
+                website = "http://reactivex.io/",
+                description = "Reactive Extensions (Rx) is a library for composing asynchronous and event-based programs using observable sequences and LINQ-style query operators."
+            },
+            new PostModel()
+            {
+                _id = 3,
+                name = "Babel",
+                website = "https://babeljs.io/",
+                description = "Babel is a compiler for writing next generation JavaScript."
+            }
+        };
+    }
+}
--- src/WebApp/Controllers/PostsController.cs
+++ src/WebApp/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Core;
 
 
 namespace WebApp.Controllers
@@ -10,42 +11,19 @@ namespace WebApp.Controllers
     [Route("api/[controller]")]
     public class PostsController : Controller
     {
-        private static List<PostModel> _posts = new List<PostModel>()
-        {
-            new PostModel()
-            {
-                _id = 1,
-                name = "Angular",
-                website = "https://angular.io/",
-                description = "Angular is a development platform for building mobile and desktop web applications."
-            },
-            new PostModel()
-            {
-                _id = 2,
-                name = "RxJs",
-                website = "http://reactivex.io/",
-                description = "Reactive Extensions (Rx) is a library for composing asynchronous and event-based programs using observable sequences and LINQ-style query operators."
-            },
-            new PostModel()
-            {
-                _id = 3,
-                name = "Babel",
-                website = "https://babeljs.io/",
-                description = "Babel is a compiler for writing next generation JavaScript."
-            }
-        };
-
+        private readonly DatabaseService _service = new DatabaseService();
 
         [HttpGet("")]
         public JsonResult ListPosts()
         {
-            return Json(_posts);
+            var posts = _service.GetPosts();
+            return Json(posts);
         }
 
         [HttpGet("{id}")]
         public JsonResult GetPost(int id)
         {
-            var post = _posts.Where(x => x._id == id).First();
+            var post = _service.FindById(id);
             return Json(post);
         }
 
@@ -53,36 +31,24 @@ namespace WebApp.Controllers
         [HttpPost("")]
         public JsonResult CreatePost([FromBody] PostRequest request)
         {
-            _posts.Add(new PostModel()
+           var post =  new PostModel()
             {
-                _id = _posts.Last()._id + 1, 
                 name = request.name,
                 website = request.website,
-                description = request.description }
-            );
-            return Json(new {success = true });
+                description = request.description 
+            };
+
+            _service.Create(post);
+            return Json(new {success = true});
         }
 
 
         [HttpPost("{id}")]
         public JsonResult UpdatePost(int id, [FromBody] PostRequest request)
         {  
-            var post = _posts.Where(x => x._id == id).First();
-
-            post.description = request.description;
-            post.name = request.name;
-            post.website = request.name;
-
+            _service.Update(id, request.name, request.description, request.website);
             return Json(new {success = true });
         }
-    }
-
-    public class PostModel 
-    {
-        public int _id {get; set;}
-        public string name {get; set;}
-        public string website {get; set;}
-        public string description {get; set;}
     }
 
     public class PostRequest
