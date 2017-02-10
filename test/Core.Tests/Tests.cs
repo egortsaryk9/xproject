cf4d12a6721390079e7d14081ed3a14a6af77a64
egortsaryk9 <egortsaryk@gmail.com>
2017-02-11 02:59:43 +0300

    Unit tests are added to 'test' folder

---
+++ test/Core.Tests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using Core;
+
+namespace CoreTests
+{
+    public class DatabaseServiceTests
+    {
+        private readonly DatabaseService _subject;
+
+        public DatabaseServiceTests()
+        {
+            _subject = new DatabaseService();
+        }
+
+        [Fact]
+        public void ShouldReturnListOfPosts() 
+        {
+            var posts = _subject.GetPosts();
+            Assert.True(posts.Count > 0);
+        }
+    }
+}
